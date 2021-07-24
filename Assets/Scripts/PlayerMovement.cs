b7d712c4dc88abe1e4db505f0be5aa5e387a8d81
Jacky Tsoi <jackytsoi2015@outlook.com>
2021-07-23 17:19:03 -0700

    Add base player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody2D rb;
+
+    public float jumpForce = 20f;
+    public Transform feet;
+    public LayerMask groundLayers;
+
+    float mx;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        mx = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButton("Jump") && IsGrounded())
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+
+        rb.velocity = movement;
+    }
+    private void Jump()
+    {
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+
+        rb.velocity = movement;
+    }
+
+    public bool IsGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+        if (groundCheck != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
